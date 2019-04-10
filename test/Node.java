4486714578bd25c413064672badc00b7ac2de152
ACER <ACER@ACER-PC>
2019-04-10 18:46:54 +0800

    i buat class dulu esk baru buat tree

---
+++ test/Node.java
@@ -0,0 +1,54 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Node<E> {
+    protected E data;
+    protected List<Node<E>> children = new ArrayList();
+    protected Node<E> parent;
+
+    public Node(E data) {
+        this.data = data;
+    }
+    
+    public Node<E> addChild(Node<E> child){
+        child.setParent(this);
+        this.children.add(child);
+        return child;
+    }
+    
+    public void addChildren(List<Node<E>> children){
+        children.forEach(each -> each.setParent(this));
+        this.children.addAll(children);
+    }
+
+    public List<Node<E>> getChildren() {
+        return children;
+    }
+
+    public E getData() {
+        return data;
+    }
+
+    public Node<E> getParent() {
+        return parent;
+    }
+
+    public void setData(E data) {
+        this.data = data;
+    }
+
+    public void setChildren(List<Node<E>> children) {
+        this.children = children;
+    }
+
+    public void setParent(Node<E> parent) {
+        this.parent = parent;
+    }
+    
+    
+    
+    
+}
