ab4a01cb763dd53e7774bcb578555c547269b3c1
LukeHuckman <LukeHuckman@users.noreply.github.com>
2019-04-21 23:17:41 +0800

    Simplified the output (might not need later due to GUI), Converted lengthy else ifs into switch, Cleanup.

---
+++ test/dsassignmenttesting/begin.java
@@ -0,0 +1,52 @@
+package dsassignmenttesting;
+
+import Main.*;
+import java.util.Scanner;
+
+public class begin {
+    public static void main(String[] args) {
+        ClassFile mlm = new ClassFile(); int mark = -1;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to main page of MLM!!!");
+        while(mark != 0){
+            System.out.print("1 Create new user."
+                    + "\n2 Retrieve the data of the user chosen."
+                    + "\n3 Update the data of the user chosen."
+                    + "\n4 Delete the user chosen."
+                    + "\n5 View the tree of the hierachy."
+                    + "\n6 Get the revenue of each generations."
+                    + "\n7 Change the registration fees."
+                    + "\n0 Close the program."
+                    + "\nEnter the input: ");
+            mark = s.nextInt();
+            switch (mark) {
+                case 1:
+                    mlm.create();
+                    break;
+                case 2://retrieve method
+                    break;
+                case 3://update method
+                    break;
+                case 4:
+                    mlm.delete();
+                    break;
+                case 5:
+                    mlm.display();
+                    break;
+                case 6://get revenue for each generations method
+                    break;
+                case 7:
+                    System.out.println("Enter new fee amount: ");
+                    double newFee = s.nextDouble();
+                    mlm.setFee(newFee);
+                    break;
+                case 0:
+                    System.out.println("The server will shut down.");
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter the input again.");
+                    break;
+            }
+        }
+    }
+}
